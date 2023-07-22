ebe8d16962f2a9a68b809d0c7b8a485f39046fd9
Eric Van de Kerckhove <blackdragon.be@gmail.com>
2023-07-22 21:12:14 +0200

    first commit

---
+++ 3d_printer_assistant.py
@@ -0,0 +1,85 @@
+import g4f
+import streamlit as st
+
+
+def fdm_material_assistant():
+	print_object_type = st.text_input(
+		"What type of object are you printing?",
+		placeholder="e.g. Vase, Raspberry Pi case, ...",
+		value="",
+	)
+
+	print_object_size = st.text_input(
+		"Approximate object size (optional)",
+		value="",
+		placeholder="e.g. 50mm x 50mm x 50mm",
+	)
+	print_object_layer_height = st.text_input(
+		"Preferred layer height (optional)", value="", placeholder="e.g. 0.2mm"
+	)
+	print_object_indoors_outdoors = st.radio(
+		"Indoors/outdoors usage", options=["Indoors", "Outdoors", "Both"]
+	)
+	print_object_functional_decorative = st.radio(
+		"Functional/decorative", options=["Functional", "Decorative", "Both"]
+	)
+	print_object_flexible = st.checkbox("Object needs to be flexible", value=False)
+
+	print_object_extra_info = st.text_input(
+		"Extra information (optional)",
+		value="",
+		placeholder="e.g. The print needs to be flexible, the print will be used in a car, ...",
+	)
+
+	if st.button("Suggest a material"):
+		MESSAGE_PREFIX = """
+		I want to 3D print an object using an FDM 3D printer. Based on the information I give, tell me what the ideal type(s) of filament would be. Some examples of filament types: PLA, PETG, TPU, TPE, ASA. Explain your answer. Also give the following recommendations for the material(s):
+		- Printing temperature
+		- Bed temperature
+		- Extra considerations
+		
+		Format your answer in markdown.
+		Object information:
+		""".strip()
+
+		message_info = """
+			- Object type: {}
+			- Object size: {}
+			- Layer height: {}
+			- Indoors/outdoors use: {}
+			- Functional/decorative: {}
+			- Object is flexible: {}
+			- Extra information: {}
+		""".format(
+			print_object_type,
+			print_object_size,
+			print_object_layer_height,
+			print_object_indoors_outdoors,
+			print_object_functional_decorative,
+			print_object_flexible,
+			print_object_extra_info,
+		)
+
+		full_message = MESSAGE_PREFIX + message_info
+
+		with st.spinner("Thinking..."):
+			response = g4f.ChatCompletion.create(
+				model=g4f.Model.gpt_4,
+				provider=g4f.Provider.ChatgptAi,
+				messages=[{"role": "user", "content": full_message}],
+			)  # alterative model setting
+		st.markdown(response)
+
+
+if __name__ == "__main__":
+	# https://docs.streamlit.io/library/api-reference/widgets
+
+	# Page setup
+	st.set_page_config(
+		page_title="3D Print Assistant", page_icon="❓", layout="wide"
+	)
+
+	st.title("3D Print Assistant")
+
+	with st.expander("FDM Material Recommendation"):
+		fdm_material_assistant()
---
+++ README.md
@@ -0,0 +1,23 @@
+# 3D Printer Assistant
+
+## What is 3D Printer Assistant?
+
+3D Printer Assistant is a web application that allows users to ask questions about 3D prints and common 3D printer issues. An AI will answer the question and provide a solution.
+
+## Installing Locally
+
+You can install 3D Printer Assistant locally using the following commands:
+
+```bash
+python -m venv .venv
+source .venv/bin/activate
+pip install -r requirements.txt
+```
+
+## Getting Started
+
+Run the application by running the following command:
+
+```bash
+streamlit run 3d_printer_assistant.py
+```
